b41aea223ec1d5d646081a9be444c83c7211fc80
g1ammi93 <giammariacarelli93@hotmail.it>
2024-02-01 16:23:33 +0000

    Added Header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'ViteComics',
+  components: {
+    AppHeader
+  }
+};
 </script>
 
+
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.container {
+  width: 1200px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,136 @@
+<script>
+
+const ItemsHeader = [
+    {
+        text: 'Characters',
+        url: '#',
+        current: false,
+    },
+    {
+        text: 'Comics',
+        url: '#',
+        current: true,
+    },
+    {
+        text: 'Movies',
+        url: '#',
+        current: false,
+    },
+    {
+        text: 'TV',
+        url: '#',
+        current: false,
+    },
+    {
+        text: 'Games',
+        url: '#',
+        current: false,
+    },
+    {
+        text: 'Collectibles',
+        url: '#',
+        current: false,
+    },
+    {
+        text: 'Videos',
+        url: '#',
+        current: false,
+    },
+    {
+        text: 'Fans',
+        url: '#',
+        current: false,
+    },
+    {
+        text: 'News',
+        url: '#',
+        current: false,
+    },
+    {
+        text: 'Shop',
+        url: '#',
+        current: false,
+    },
+];
+
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            ItemsHeader,
+        };
+    },
+    methods: {
+        selectedItem(index) {
+            this.ItemsHeader.forEach((item, i) => {
+                item.current = i === index;
+            });
+
+
+        }
+    }
+};
+</script>
+
+
+<template>
+    <header>
+        <div class="container">
+            <img src="../assets/img/dc-logo.png" alt="Logo Dc comics">
+            <ul>
+                <li v-for="(item, index) in ItemsHeader" :key="item.text">
+                    <a :href="item.url" :class="{ current: item.current }" @click="selectedItem(index)">{{ item.text
+                    }}</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+header {
+    background-color: white;
+}
+
+div {
+    margin-top: 20px;
+    height: 80px;
+    background-color: gray;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+ul {
+    display: flex;
+    gap: 30px;
+    list-style: none;
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: small;
+}
+
+img {
+    width: 70px;
+}
+
+a {
+    text-decoration: none;
+    color: black;
+}
+
+a.current {
+    position: relative;
+    text-decoration: none;
+}
+
+a.current::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: -33px;
+    width: 100%;
+    height: 4px;
+    background-color: dodgerblue;
+}
+</style>
