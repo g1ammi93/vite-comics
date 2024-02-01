647da69efe8a8cf713cb59c9b0b2da3d221fb4a8
g1ammi93 <giammariacarelli93@hotmail.it>
2024-02-01 19:25:22 +0000

    Added Footer Content

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,15 +6,163 @@ export default {
 
 <template>
     <footer>
-        <div>
-            <h1>ciao ciao</h1>
+        <div class="image-bkg">
+            <div class="space-container container">
+                <div class="flex-row">
+                    <div class="col">
+                        <ul>
+                            <li>
+                                <h3>DC COMICS</h3>
+                            </li>
+                            <li><a>Characters</a></li>
+                            <li><a>Characters</a></li>
+                            <li><a>Characters</a></li>
+                            <li><a>Characters</a></li>
+                            <li><a>Characters</a></li>
+                            <li><a>Characters</a></li>
+                            <li><a>Characters</a></li>
+
+                        </ul>
+                        <ul>
+                            <li>
+                                <h3>SHOP</h3>
+                            </li>
+                            <li><a>Characters</a></li>
+                            <li><a>Characters</a></li>
+
+                        </ul>
+                    </div>
+                    <ul class="col">
+                        <li>
+                            <h3>DC</h3>
+                        </li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+
+                    </ul>
+                    <ul class="col">
+                        <li>
+                            <h3>SITES</h3>
+                        </li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+                        <li><a>Characters</a></li>
+
+                    </ul>
+
+
+                </div>
+                <div>
+                    <img class="img-dc" src="../assets/img/dc-logo-bg.png" alt="Logo Dc">
+                </div>
+            </div>
         </div>
 
+    </footer>
+    <footer class="lower-footer">
+        <div class="container space-between">
+            <div>
+                <button>SIGN-UP NOW!</button>
+            </div>
+            <div class="social-media">
+                <span>FOLLOW US</span>
+                <a><img src="../assets/img/footer-facebook.png" alt="Facebook"></a>
+                <a><img src="../assets/img/footer-twitter.png" alt="Twitter"></a>
+                <a><img src="../assets/img/footer-youtube.png" alt="Youtube"></a>
+                <a><img src="../assets/img/footer-pinterest.png" alt="Pinterest"></a>
+                <a><img src="../assets/img/footer-periscope.png" alt="Periscope"> </a>
+
+            </div>
+
+        </div>
     </footer>
 </template>
 
 <style scoped>
 h1 {
     color: red;
+}
+
+.image-bkg {
+    background-image: url('../assets/img/footer-bg.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 400px;
+}
+
+h3 {
+    color: white;
+    margin-bottom: 10px;
+}
+
+ul {
+    margin-top: 30px;
+    list-style: none;
+}
+
+.space-container {
+    display: flex;
+    justify-content: space-between;
+}
+
+.flex-row {
+    display: flex;
+    gap: 40px;
+}
+
+.col {
+    display: flex;
+    flex-direction: column;
+}
+
+.img-dc {
+    width: 380px;
+}
+
+button {
+    padding: 10px;
+    background-color: transparent;
+    color: white;
+    border: 1px solid dodgerblue;
+    cursor: pointer;
+}
+
+span {
+    color: dodgerblue;
+    text-wrap: nowrap;
+}
+
+.space-between {
+    display: flex;
+    justify-content: space-between;
+}
+
+.lower-footer {
+    height: 100px;
+    background-color: black;
+    display: flex;
+    align-items: center;
+}
+
+.social-media {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+}
+
+a {
+    cursor: pointer;
 }
 </style>
